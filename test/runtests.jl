a7f3c4867aa1f33ab36e26a7a2e91deb1a0a07f2
Rob J Goedman <goedman@mac.com>
2020-12-22 09:38:38 -0700

    Rel 2.1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,44 +1,50 @@
 using StanDiagnose
 using Test
 
-ProjDir = dirname(@__FILE__)
+if haskey(ENV, "JULIA_CMDSTAN_HOME")
 
-bernoulli_model = "
-data { 
-  int<lower=0> N; 
-  int<lower=0,upper=1> y[N];
-} 
-parameters {
-  real<lower=0,upper=1> theta;
-} 
-model {
-  theta ~ beta(1,1);
-    y ~ bernoulli(theta);
-}
-"
-bernoulli_data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
+  ProjDir = dirname(@__FILE__)
 
-tmpdir = joinpath(@__DIR__, "tmp")
+  bernoulli_model = "
+  data { 
+    int<lower=0> N; 
+    int<lower=0,upper=1> y[N];
+  } 
+  parameters {
+    real<lower=0,upper=1> theta;
+  } 
+  model {
+    theta ~ beta(1,1);
+      y ~ bernoulli(theta);
+  }
+  "
+  bernoulli_data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
 
-@testset "Bernoulli diagnose" begin
+  tmpdir = joinpath(@__DIR__, "tmp")
 
-  stanmodel = DiagnoseModel("bernoulli", bernoulli_model;
-    method=StanDiagnose.Diagnose(StanDiagnose.Gradient(epsilon=1e-6)));
-  rc = stan_diagnose(stanmodel; data=bernoulli_data);
+  @testset "Bernoulli diagnose" begin
 
-  if success(rc)
-    diags = read_diagnose(stanmodel)
-    tmp = diags[:error][1]
-    @test round.(tmp, digits=6) ≈ 0.0
+    stanmodel = DiagnoseModel("bernoulli", bernoulli_model;
+      method=StanDiagnose.Diagnose(StanDiagnose.Gradient(epsilon=1e-6)));
+    rc = stan_diagnose(stanmodel; data=bernoulli_data);
+
+    if success(rc)
+      diags = read_diagnose(stanmodel)
+      tmp = diags[:error][1]
+      @test round.(tmp, digits=6) ≈ 0.0
+    end
+    
+    stanmodel = DiagnoseModel("bernoulli", bernoulli_model;
+      method=StanDiagnose.Diagnose(StanDiagnose.Gradient(epsilon=1e-8)));
+    rc2 = stan_diagnose(stanmodel; data=bernoulli_data);
+
+    if success(rc2)
+      diags = read_diagnose(stanmodel)
+      tmp = diags[:error][1]
+      @test round.(tmp, digits=6) ≈ 0.0
+    end
   end
-  
-  stanmodel = DiagnoseModel("bernoulli", bernoulli_model;
-    method=StanDiagnose.Diagnose(StanDiagnose.Gradient(epsilon=1e-8)));
-  rc2 = stan_diagnose(stanmodel; data=bernoulli_data);
 
-  if success(rc2)
-    diags = read_diagnose(stanmodel)
-    tmp = diags[:error][1]
-    @test round.(tmp, digits=6) ≈ 0.0
-  end
+else
+  println("\nJULIA_CMDSTAN_HOME not set. Skipping tests")
 end

04b2fd2d6de21fe9ee7944f33345a7c595b99f0c
Rob J Goedman <goedman@icloud.com>
2021-12-04 07:37:58 -0700

    Rel 3.1.2 - Allow CMDSTAN in testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using StanDiagnose
 using Test
 
-if haskey(ENV, "JULIA_CMDSTAN_HOME")
+if haskey(ENV, "JULIA_CMDSTAN_HOME") || haskey(ENV, "CMDSTAN")
 
   ProjDir = dirname(@__FILE__)
 
@@ -42,5 +42,5 @@ if haskey(ENV, "JULIA_CMDSTAN_HOME")
   end
 
 else
-  println("\nJULIA_CMDSTAN_HOME not set. Skipping tests")
+  println("\nCMDSTAN or JULIA_CMDSTAN_HOME not set. Skipping tests")
 end
